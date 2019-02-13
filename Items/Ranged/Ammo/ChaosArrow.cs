66dd81fa3fd23da599247c29ac055b16b881a482
VVV101 <VVVHUNTER@yandex.ru>
2019-02-14 00:32:13 +0600

    Some ammo
    Chaos Arrow, Dragonfire Arrow/Bullet/Dart, Hydratoxin Arrow/Bullet/Dart, Order Arrow, Coin Pistol.

---
+++ Items/Ranged/Ammo/ChaosArrow.cs
@@ -0,0 +1,41 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AAMod.Items.Ranged.Ammo
+{
+    public class ChaosArrow : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Chaos Arrow");
+            DisplayName.SetDefault("Inflicts randomly selected debuff on hit");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 20;
+			item.ranged = true;
+			item.width = 14;
+			item.height = 32;
+			item.maxStack = 999;
+			item.consumable = true;             //You need to set the item consumable so that the ammo would automatically consumed
+			item.knockBack = 4f;
+			item.value = 30;
+			item.rare = 1;
+			item.shoot = mod.ProjectileType("ChaosArrow");   //The projectile shoot when your weapon using this ammo
+			item.shootSpeed = 1f;                  //The speed of the projectile
+			item.ammo = AmmoID.Arrow;              //The ammo class this ammo belongs to.
+		}
+
+       public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.WoodenArrow, 150);
+            recipe.AddIngredient(mod, "ChaosCrystal", 1);
+            recipe.AddTile(TileID.MythrilAnvil);
+			recipe.SetResult(this, 150);
+			recipe.AddRecipe();
+		}
+	}
+}
